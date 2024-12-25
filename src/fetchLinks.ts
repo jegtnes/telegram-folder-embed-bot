7b568bf1f16a84c21addc4cb8d8d4cedf55d4e5f
Alex Jegtnes <alex@jegtnes.com>
2024-12-25 16:08:42 +0000

    Install and apply Biome

--- src/fetchLinks.ts
+++ src/fetchLinks.ts
@@ -1,29 +1,26 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 export async function fetchLinks(url: string): Promise<string[]> {
-    try {
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-        
-        const links: string[] = $('a[href]')
-            .map((i, element) => {
-                if(!element) return;
-                const href = $(element).attr('href') || "";
-                const fullLink = new URL(href, url);
-                return fullLink.href;
-            })
-            .get();
+	try {
+		const response = await axios.get(url);
+		const $ = cheerio.load(response.data);
 
-        return links;
-    } catch (error: any) {
-        console.error(`fucked it m8: ${error.message}`);
-        return [];
-    }
+		const links: string[] = $("a[href]")
+			.map((i, element) => {
+				if (!element) return;
+				const href = $(element).attr("href") || "";
+				const fullLink = new URL(href, url);
+				return fullLink.href;
+			})
+			.get();
+		return links;
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			console.error(`fucked it m8: ${error?.message}`);
+		} else {
+			console.error("fucked it so hard this isn't even an error");
+		}
+		return [];
+	}
 }
-
-(async () => {
-    const links = await fetchLinks('https://folder.jegtnes.com');
-    console.debug(links);
-    return;
-})();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Context, NarrowedContext, Telegraf } from 'telegraf';
-import { message } from 'telegraf/filters';
-import { config } from 'dotenv';
-import { Update } from "telegraf/types";
+import { config } from "dotenv";
+import { type Context, type NarrowedContext, Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import type { Update } from "telegraf/types";
 
 type InlineQueryContext = NarrowedContext<Context, Update.InlineQueryUpdate>;
 
@@ -9,42 +9,45 @@ config();
 
 const botKey: string = process.env.BOT_TOKEN || "";
 const domain: string = process.env.DOMAIN || "";
-const port: number = parseInt(process.env.PORT || '0', 10);
+const port: number = Number.parseInt(process.env.PORT || "0", 10);
 
 console.info(`Started bot process at port ${port} at domain ${domain}`);
 
 const bot = new Telegraf(botKey);
 
-bot.start((ctx: Context) => ctx.reply('Welcome'));
+bot.start((ctx: Context) => ctx.reply("Welcome"));
 bot.help((ctx: Context) => {
-    ctx.reply('Send me a sticker')
+	ctx.reply("Send me a sticker");
 });
 
-bot.on(message('sticker'), (ctx: Context) => {
-    console.info('Received sticker');
-    ctx.reply('👍')
+bot.on(message("sticker"), (ctx: Context) => {
+	console.info("Received sticker");
+	ctx.reply("👍");
 });
 
-bot.hears('hi', (ctx: Context) => {
-    console.info('Received "hi"');
-    ctx.reply('Hey there')
-})
+bot.hears("hi", (ctx: Context) => {
+	console.info('Received "hi"');
+	ctx.reply("Hey there");
+});
 
-bot.on('inline_query', async (ctx: InlineQueryContext) => {
-    console.info('Received folder inline query command: ', ctx.update.inline_query.query);
+bot.on("inline_query", async (ctx: InlineQueryContext) => {
+	console.info(
+		"Received folder inline query command: ",
+		ctx.update.inline_query.query,
+	);
 
-    const result = []
-    return await ctx.answerInlineQuery(result)
+	const result = [];
+	return await ctx.answerInlineQuery(result);
 });
 
 // Start webhook via launch method (preferred)
 bot.launch({
-    webhook: {
-        domain: domain,
-        port: port,
-    },
+	webhook: {
+		domain: domain,
+		port: port,
+	},
 });
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
